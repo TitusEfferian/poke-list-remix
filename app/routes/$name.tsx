6e05c54690e3689e2515f23f4deb549da53e1d37
Titus Efferian <kiritosato@gmail.com>
2023-04-05 07:53:14 +0700

    indent

--- app/routes/$name.tsx
+++ app/routes/$name.tsx
@@ -4,25 +4,25 @@ import { json } from "@remix-run/node";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 
 interface LoaderProps {
-    name: string;
-    sprites: {
-        front_default: string;
-        back_default: string;
-    }
-    types: {
-        type: {
-            name: string
-        }
-    }[]
+	name: string;
+	sprites: {
+		front_default: string;
+		back_default: string;
+	}
+	types: {
+		type: {
+			name: string
+		}
+	}[]
 }
 
 export const loader = async ({ params }: LoaderArgs) => {
-    const fetchData = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
-    if (fetchData.status !== 200) {
-        throw new Error();
-    }
-    const fetchJson = await fetchData.json();
-    return json(fetchJson);
+	const fetchData = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+	if (fetchData.status !== 200) {
+		throw new Error();
+	}
+	const fetchJson = await fetchData.json();
+	return json(fetchJson);
 }
 
 export function ErrorBoundary() {
@@ -38,43 +38,43 @@ export function ErrorBoundary() {
 }
 
 const PokeDetail = () => {
-    const pokeData = useLoaderData() as LoaderProps;
-    const navigate = useNavigate();
-    return (
-        <Container>
-            <Stack>
-                <Card mt={'lg'}>
-                    <Card.Section>
-                        <Group position="center">
-                            <Image
-                                withPlaceholder
-                                width={120}
-                                height={120}
-                                src={pokeData.sprites.front_default}
-                                alt={pokeData.name}
-                            />
-                            <Image
-                                withPlaceholder
-                                width={120}
-                                height={120}
-                                src={pokeData.sprites.back_default}
-                                alt={pokeData.name}
-                            />
-                        </Group>
-                    </Card.Section>
-                    <Stack>
-                        <Title order={2}>{pokeData.name}</Title>
-                        <Group>
-                            {pokeData.types.map(x => {
-                                return <Badge key={x.type.name}>{x.type.name}</Badge>
-                            })}
-                        </Group>
-                    </Stack>
-                </Card>
-                <Button onClick={() => { navigate(-1) }}>ok</Button>
-            </Stack>
-        </Container>
-    )
+	const pokeData = useLoaderData() as LoaderProps;
+	const navigate = useNavigate();
+	return (
+		<Container>
+			<Stack>
+				<Card mt={'lg'}>
+					<Card.Section>
+						<Group position="center">
+							<Image
+								withPlaceholder
+								width={120}
+								height={120}
+								src={pokeData.sprites.front_default}
+								alt={pokeData.name}
+							/>
+							<Image
+								withPlaceholder
+								width={120}
+								height={120}
+								src={pokeData.sprites.back_default}
+								alt={pokeData.name}
+							/>
+						</Group>
+					</Card.Section>
+					<Stack>
+						<Title order={2}>{pokeData.name}</Title>
+						<Group>
+							{pokeData.types.map(x => {
+								return <Badge key={x.type.name}>{x.type.name}</Badge>
+							})}
+						</Group>
+					</Stack>
+				</Card>
+				<Button onClick={() => { navigate(-1) }}>ok</Button>
+			</Stack>
+		</Container>
+	)
 }
 
 export default PokeDetail;
